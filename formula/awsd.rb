0a5a1e9cc51ebe25ba847376a600cf45acf5c3c1
Patrick Joyce <pjaudiomv@gmail.com>
2021-11-29 16:04:05 -0500

    update sha

--- formula/awsd.rb
+++ formula/awsd.rb
@@ -1,22 +1,22 @@
 class Awsd < Formula
-  ver = "0.0.1"
-  
-  desc "AWS Profile Switcher in Go"
-  homepage "https://github.com/pjaudiomv/awsd"
-  url "https://github.com/pjaudiomv/awsd/archive/v0.0.1.tar.gz"
-  sha256 "29bc20c763416f22dc9fc0857c389e4178d3f7c7d9e5b2bac3b24b7a329de685"
-  license "MIT"
-  head "https://github.com/pjaudiomv/awsd.git", branch: "main", :tag => ver
-  version ver
+    ver = "0.0.2"
+    desc "AWS Profile Switcher in Go"
+    homepage "https://github.com/pjaudiomv/awsd"
+    url "https://github.com/pjaudiomv/awsd/archive/v0.0.2.tar.gz"
+    sha256 "8efd05b525e48aa0e29ba788cc92c39bdc67ecc92ac5a1fd0b281455b632d3d8"
+    license "MIT"
+    head "https://github.com/pjaudiomv/awsd.git", branch: "main", :tag => ver
+    version ver
 
-  depends_on "go" => :build
+    depends_on "go" => :build
 
-  def installs
-    system "PREFIX=#{share}", "bash", "install.sh"
-    bin.install Dir["#{share}/bin/*"]
-  end
+    def install
+        system "make", "install"
+        bin.install "bin/_awsd_prompt"
+        bin.install "bin/_awsd"
+    end
 
-  test do
-    system "true"
-  end
+    test do
+        system "true"
+    end
 end
