9ef2fb21c8c98bb697de4793a37e5a504d752e1b
Patrick Joyce <pjaudiomv@gmail.com>
2021-11-29 16:39:56 -0500

    update sha

--- formula/awsd.rb
+++ formula/awsd.rb
@@ -3,7 +3,7 @@ class Awsd < Formula
     desc "AWS Profile Switcher in Go"
     homepage "https://github.com/pjaudiomv/awsd"
     url "https://github.com/pjaudiomv/awsd/archive/v0.0.2.tar.gz"
-    sha256 "b53270b6a8487cb7ff8dce64b16b878479c024c756cd9f4305638671b64ad68d"
+    sha256 "5fc4107035e716cfb208f4191648d2d53fd5fb491017064783aad447abd5dd3a"
     license "MIT"
     head "https://github.com/pjaudiomv/awsd.git", branch: "main", :tag => ver
     version ver
@@ -11,7 +11,7 @@ class Awsd < Formula
     depends_on "go" => :build
 
     def install
-        ENV["prefix"] = "#{prefix}"
+        ENV["DESTDIR"] = "#{prefix}"
         system "make", "install"
         bin.install "bin/_awsd_prompt"
         bin.install "bin/_awsd"
