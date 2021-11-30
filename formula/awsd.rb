7136891ceef7f3cb1f804bcffa4fc3235aad8169
Patrick Joyce <pjaudiomv@gmail.com>
2021-11-30 10:04:08 -0500

    add repo back to tap

--- formula/awsd.rb
+++ formula/awsd.rb
@@ -1,10 +1,12 @@
 class Awsd < Formula
+    ver = "0.0.1"
     desc "AWS Profile Switcher in Go"
     homepage "https://github.com/pjaudiomv/awsd"
     url "https://github.com/pjaudiomv/awsd/archive/v0.0.1.tar.gz"
     sha256 "b2cd0e48abc6e36908c37e24ed7d1406ad20c6efda52ac11ec52e766dff8ed86"
     license "MIT"
-    version "0.0.1"
+    head "https://github.com/pjaudiomv/awsd.git", branch: "main", :tag => ver
+    version ver
 
     depends_on "go" => :build
 
