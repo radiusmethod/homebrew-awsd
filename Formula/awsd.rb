fb62c4d1326d027d33ae73fa5ed1ad8c2931929e
Patrick Joyce <pjaudiomv@gmail.com>
2023-10-20 21:32:13 -0400

    changes for org mv

--- Formula/awsd.rb
+++ Formula/awsd.rb
@@ -1,11 +1,11 @@
 class Awsd < Formula
     ver = "v0.0.6"
     desc "AWS Profile Switcher in Go"
-    homepage "https://github.com/pjaudiomv/awsd"
-    url "https://github.com/pjaudiomv/awsd/archive/v0.0.6.tar.gz"
+    homepage "https://github.com/radiusmethod/awsd"
+    url "https://github.com/radiusmethod/awsd/archive/v0.0.6.tar.gz"
     sha256 "95aa25706da0bef6c6af75b1322ce9b2ddea1b73a3aafff9ddaf585dab5e718d"
     license "MIT"
-    head "https://github.com/pjaudiomv/awsd.git", branch: "main", :tag => ver
+    head "https://github.com/radiusmethod/awsd.git", branch: "main", :tag => ver
     version ver
 
     depends_on "go" => :build
