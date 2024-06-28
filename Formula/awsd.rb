f2840a64a82332f68e7f6ac15852420b7facc83e
Patrick Joyce <pjaudiomv@gmail.com>
2024-06-28 08:25:04 -0400

    homebrew tests

--- Formula/awsd.rb
+++ Formula/awsd.rb
@@ -26,7 +26,13 @@ class Awsd < Formula
       EOS
     end
 
-    test do
-        system "true"
-    end
+  test do
+    assert_match "awsd version: #{version}", shell_output("#{bin}/_awsd_prompt version")
+    assert_predicate bin/"_awsd_prompt", :exist?, "_awsd_prompt binary should exist"
+    assert_predicate bin/"_awsd_prompt", :executable?, "_awsd_prompt binary should be executable"
+    assert_predicate bin/"_awsd", :exist?, "_awsd script should exist"
+    assert_predicate bin/"_awsd", :executable?, "_awsd script should be executable"
+    assert_predicate bin/"_awsd_autocomplete", :exist?, "_awsd_autocomplete script should exist"
+    assert_predicate bin/"_awsd_autocomplete", :executable?, "_awsd_autocomplete script should be executable"
+  end
 end
